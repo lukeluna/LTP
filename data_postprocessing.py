b3182fb79f44ff642555e9835945870a90df6cdb
lukeluna <49711652+lukeluna@users.noreply.github.com>
2024-06-04 14:31:22 +0200

    Added comments and cleaned up, added SC

--- data_postprocessing.py
+++ data_postprocessing.py
@@ -1,17 +1,27 @@
 import re
-import numpy
 import pandas as pd
-import random
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, f1_score
+from collections import Counter
 
+# important to know before you run:
+# - change the file path to the data directory
+# - change the file name to the data file name
+# - change the self_consistency value according to the dataset you are running on
+# - comment or uncomment the answer line in find_fallacy depending on the prompt type
+
+# Used to perform regex on an input string
 def contains_whole_word(large_string, word):
     pattern = rf'\b{re.escape(word)}\b'
     return bool(re.search(pattern, large_string))
 
+
+# Equivalence dictionary that can be used to relabel model outputs that are paraphrased or use synonyms
 fallacy_types = {
     'ad hominem': {'ad hominem'},
-    'ad populum': {'ad populum', 'appeal to popularity', 'appeal to majority', 'bandwagon', 'appeal to popular belief', 'consensus', 'majority'},
-    'appeal to (false) authority': {'appeal to (false) authority', 'appeal to false authority', 'appeal to authority', 'misplaced authority', 'fallacy of authority', 'authority'},
+    'ad populum': {'ad populum', 'appeal to popularity', 'appeal to majority', 'bandwagon', 'appeal to popular belief',
+                   'consensus', 'majority'},
+    'appeal to (false) authority': {'appeal to (false) authority', 'appeal to false authority', 'appeal to authority',
+                                    'misplaced authority', 'fallacy of authority', 'authority'},
     'appeal to anger': {'appeal to anger', 'appeal to emotion (anger)', 'anger appeal', 'anger'},
     'appeal to fear': {'appeal to fear', 'appeal to emotion (fear)', 'fear appeal', 'fear'},
     'appeal to nature': {'appeal to nature'},
@@ -26,7 +36,8 @@ fallacy_types = {
     'equivocation': {'equivocation', 'vagueness'},
     'fallacy of division': {'fallacy of division'},
     'false analogy': {'false analogy', 'analogy'},
-    'false causality': {'false causality', 'false cause', 'correlation does not imply causation', 'causality', 'post hoc'},
+    'false causality': {'false causality', 'false cause', 'correlation does not imply causation', 'causality',
+                        'post hoc'},
     'false dilemma': {'false dilemma', 'black-and-white', 'black-or-white', 'black and white', 'black or white'},
     'guilt by association': {'guilt by association'},
     'hasty generalization': {'hasty generalization', 'generalization', 'generalizability'},
@@ -37,75 +48,111 @@ fallacy_types = {
 
 }
 
+
+# Function that takes in an input text and outputs the fallacies detected as a list or 'no match'
 def find_fallacy(input_sentence):
     # Normalize the input sentence to lowercase for case-insensitive matching
     input_sentence = input_sentence.lower()
     answer = ''
-    pattern = r'answer:\s*(.*)' #TODO: this now requires the format answer: ... so it does not work for RaR or CoT
 
+    # Searches for the entire sentence that follows after 'answer', used for CoT, CoT-SC and ToT
+    # to avoid taking fallacy labels in the output into account that are not part of the final
+    # conclusion of the model
+    pattern = r'answer\s*(.*)'
+    matches = []
+    no_match = ['no match']
     # Perform the search
     match = re.search(pattern, input_sentence)
     if match:
         answer = match.group(1)
 
-    #answer = input_sentence #uncomment this if you want to run on the entire input
+    # For RaR, uncomment the following line
+    # answer = input_sentence #uncomment this if you want to run on the entire input
 
     # Check each fallacy and its synonyms
     for canonical, synonyms in fallacy_types.items():
-        # Create a regex pattern that matches any of the synonyms
+
+        # Create a regex pattern that matches any of the synonyms in the equivalence dictionary
         pattern = r'\b(' + '|'.join(re.escape(syn) for syn in synonyms) + r')\b'
 
-        # Search for the pattern in the input sentence
+        # Search for the pattern in the input text (either the sentence after answer or the whole model output)
         if re.search(pattern, answer):
-            return canonical, answer  #TODO: option to find multiple classes in answer
 
-    # Return None if no fallacy type is found
-    return 'no match', answer
+            # Only append new matches if the match has not yet been found (so there will be no duplicates)
+            if canonical not in matches:
+                # Create a list of all detected fallacies in the text
+                matches.append(canonical)
 
-path = '/Users/lukevandenwittenboer/PycharmProjects/LTP/test/'
+    # Return the list of matches or 'no match'
+    return matches if matches else no_match
 
-results = pd.read_csv(path + 'results/results-mafalda/results-tot-gemma.csv')
 
-actual_labels = results['actual_label']
-expected_labels = results['expected_label']
+# Change the path to the data directory and change the filename to the filename of the data (without extension)
+path = '/Users/lukevandenwittenboer/PycharmProjects/LTP/test/results/results-mafalda/'
+filename = 'results-cot-sc-openchat'
+self_concistency = True  # Turn True if testing cot-cs, else turn False
+results = pd.read_csv(path + filename + '.csv')
 
-processed_labels = []
-test_results = []
+if self_concistency is True:
 
-# Go through all sentences
-for index, row in results.iterrows():
-    model_output = row['actual_label']
+    # Iterate through all results
+    for index, row in results.iterrows():
 
-    # Check which fallacy was detected
-    detected_label, answer = find_fallacy(model_output)
+        labels = []
+        # Find the labels in the three model answers for each statement for Self Consistency
+        labels.extend(find_fallacy(row['actual_label1']))
+        labels.extend(find_fallacy(row['actual_label2']))
+        labels.extend(find_fallacy(row['actual_label3']))
 
-    # Print fallacies that were not detected (for analysis testing only)
-    # if detected_label is 'no match':
-    #     if row['expected_label'] != 'nothing':
-    #         print(answer + '   correct: ' + row['expected_label'])
-    processed_labels.append(detected_label)
-    if detected_label == row['expected_label']:
-        test_results.append(True)
-    else:
-        test_results.append(False)
-        print(detected_label + '   correct: ' + row['expected_label'])
+        # Count the total amount of individual labels detected in each model output
+        counts = Counter(labels)
 
+        # If there are two or more labels that are identical, it
+        matches = [label for label, count in counts.items() if count >= 2]
+
+        # If no items appear twice or more, add the first label to matches
+        if not matches and labels:  # Also checking if labels is not empty
+            matches.append(labels[0])
+
+        # Add the canonical labels to the results dataframe
+        results.at[index, 'canonical'] = matches
+else:
+    # Go through all model outputs and detect the labels, add the canonical labels to the results dataframe
+    results['canonical'] = results['actual_label'].apply(lambda x: find_fallacy(x))
+
+# Split instances that have multiple matches into different rows in the dataframe
+results = results.explode('canonical')
+
+# Drop all instances that have n/a in the dataframe (usually not the case)
+results = results.dropna(subset=['canonical'])
+
+# Print the head of the dataframe (testing only)
+print(results.head())
+
+# Write the results including the canonical detected labels to a csv file
+results.to_csv(path + filename + '_canonical.csv', index=False)
+
+# Go through all sentences (testing only)
+# for index, row in results.iterrows():
+#     model_output = row['actual_label']
 #
+#     # Check which fallacy was detected
+#     detected_label = find_fallacy(model_output)
+#
+#     # Print fallacies that were not detected (for analysis testing only)
+#     # if detected_label is 'no match':
+#     #     if row['expected_label'] != 'nothing':
+#     #         print(answer + '   correct: ' + row['expected_label'])
+#     processed_labels.append(detected_label)
+#     if detected_label == row['expected_label']:
+#         test_results.append(True)
+#     else:
+#         test_results.append(False)
+#         print(detected_label + '   correct: ' + row['expected_label'])
+
 # Calculate metrics
-accuracy = accuracy_score(expected_labels, processed_labels)
-precision = precision_score(expected_labels, processed_labels, average='weighted')  # adjust average method as necessary
-recall = recall_score(expected_labels, processed_labels, average='weighted')  # adjust average method as necessary
-f1 = f1_score(expected_labels, processed_labels, average='weighted')  # adjust average method as necessary
-#
+accuracy = accuracy_score(results['expected_label'], results['canonical'])
+f1 = f1_score(results['expected_label'], results['canonical'], average='weighted')  # adjust average method as necessary
 
-print(accuracy, f1, precision, recall)
-
-# #
-# accuracy = sum(test_results) / len(test_results)
-# f1 = f1_score([True] * len(test_results), test_results, average='weighted')
-# precision = precision_score([True] * len(test_results), test_results, average='weighted')
-# recall = recall_score([True] * len(test_results), test_results, average='weighted')
-#
-# print(accuracy, f1, precision, recall)
-
-
+# Print the performance metrics
+print(filename + '\t', accuracy, '\t', f1)
